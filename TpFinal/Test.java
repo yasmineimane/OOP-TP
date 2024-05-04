49f76bc696aca39c27a67b03ac97597f3f462a6d
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-05-04 18:28:17 +0200

    new

--- TpFinal/Test.java
+++ TpFinal/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args)
     {
         UC uc = new UC(3.2, 3, 150);
-        Clavier clavier = new Clavier(1, "KB", 10, 5);
+        Clavier clavier = new Clavier(1, "KB", 102, 5);
         Monitor monitor = new Monitor(1, "MN", 2, 18, 1700);
         Peripheriques p = new Peripheriques();
 
